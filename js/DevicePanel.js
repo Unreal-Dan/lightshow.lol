8502d7e0d4c0893343ef2ffcd0c59026c51065ba
Dan <UnrealDanMail@gmail.com>
2025-03-09 09:17:37 -0700

    mobile device panel fix

--- js/DevicePanel.js
+++ js/DevicePanel.js
@@ -31,7 +31,7 @@ export default class DevicePanel extends Panel {
               <i class="fa-solid fa-xmark"></i>
             </button>
           </div>
-          <div id="deviceInfoPanelContent">
+          <div id="deviceInfoPanelContent" style="display:none;">
             <div id="brightnessControl">
               <input type="range" id="brightnessSlider" min="0" max="255" step="1" value="255" />
               <i class="fa-solid fa-sun" id="brightnessIcon"></i>
