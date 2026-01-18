0b5b9e7885a228c6b31d7155ecce185bdf5a86db
Dan <UnrealDanMail@gmail.com>
2026-01-18 00:05:39 -0800

    updated cache buster script

--- add_cache_busters.sh
+++ add_cache_busters.sh
@@ -17,7 +17,7 @@ echo "Using cache-buster: $CACHE_BUSTER"
 #   <script type="module" src="js/VortexEditor.js"></script>
 # to
 #   <script type="module" src="js/VortexEditor.js?v=abc123"></script>
-sed -i.bak "s|src=\"js/VortexEditor.js\"|src=\"js/VortexEditor.js$CACHE_BUSTER\"|g" ./index.html
+sed -i.bak "s|src=\"js/Main.js\"|src=\"js/Main.js$CACHE_BUSTER\"|g" ./index.html
 rm -f ./index.html.bak
 
 
@@ -39,6 +39,18 @@ for cssFile in ./css/*.css; do
   rm -f "$cssFile.bak"
 done
 
+# also do the mobile css files
+for cssFile in ./css/mobile/*.css; do
+  # Append ?v=... to @import references
+  # Matches: @import url('something.css')  or  @import url("something.css")
+  # Replaces with: @import url('something.css?v=abc123')
+  sed -i.bak "s|\(@import[[:space:]]\+url([\"'][^\"']*\.css\)|\1$CACHE_BUSTER|g" "$cssFile"
+
+  # Replace any manual placeholders __CACHE_BUSTER__
+  sed -i.bak "s|__CACHE_BUSTER__|$CACHE_BUSTER|g" "$cssFile"
+
+  rm -f "$cssFile.bak"
+done
 
 ############################################
 # 3) Update all JS in ./js/*.js except VortexLib.js
@@ -66,5 +78,22 @@ for jsFile in ./js/*.js; do
   rm -f "$jsFile.bak"
 done
 
-echo "Cache-busting complete."
+# also do the mobile js files
+for jsFile in ./js/mobile/*.js; do
+  # Skip VortexLib.js
+  if [[ "$jsFile" == *"VortexLib.js" ]]; then
+    continue
+  fi
 
+  # Append ?v=... to ES import statements.
+  # This will look for: import ... from "<anything>.js"
+  # and change it to:   import ... from "<anything>.js?v=abc123"
+  sed -i.bak "s|\(\import[[:space:]][^;]* from[[:space:]]*[\"'][^\"']*\.js\)\([\"']\)|\1$CACHE_BUSTER\2|g" "$jsFile"
+
+  # Replace any manual placeholders __CACHE_BUSTER__
+  sed -i.bak "s|__CACHE_BUSTER__|$CACHE_STR|g" "$jsFile"
+
+  rm -f "$jsFile.bak"
+done
+
+echo "Cache-busting complete."
