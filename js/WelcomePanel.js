6fb79dd5298d8c6713035c01bb9c84e2ace44ded
Unreal-Dan <unrealdanmail@gmail.com>
2026-07-09 04:05:08 -0700

    fix: remove inline width override from floatPanel so CSS width applies

--- js/WelcomePanel.js
+++ js/WelcomePanel.js
@@ -149,7 +149,8 @@ export default class WelcomePanel extends Panel {
   show() {
     super.show();
     if (!this.editor.detectMobile()) {
-      this.editor.dockManager.floatPanel(this.id, (window.innerWidth - 800) / 2, 50);
+      this.editor.dockManager.floatPanel(this.id, Math.max(10, (window.innerWidth - 800) / 2), 50);
+      this.panel.style.width = '';
     }
   }
 
