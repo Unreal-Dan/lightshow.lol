c333bda6b7727a75533ce4d6b568cbdc6f0106d0
Unreal-Dan <unrealdanmail@gmail.com>
2026-07-09 04:26:51 -0700

    fix: override floating-panel max-width with !important

--- js/WelcomePanel.js
+++ js/WelcomePanel.js
@@ -151,7 +151,6 @@ export default class WelcomePanel extends Panel {
     if (!this.editor.detectMobile()) {
       this.editor.dockManager.floatPanel(this.id, Math.max(10, (window.innerWidth - 900) / 2), 30);
       this.panel.style.width = '';
-      this.panel.style.height = '';
     }
   }
 
